b7e3bb5c5e37e642327d208da2e1e985380314c8
Laurent <laurent.simplon@gmail.com>
2017-03-09 09:10:19 +0100

    Avec les tests OK

--- src/main/java/co/simplon/dates/DecouverteDates.java
+++ src/main/java/co/simplon/dates/DecouverteDates.java
@@ -1,6 +1,9 @@
 package co.simplon.dates;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DecouverteDates {
@@ -10,33 +13,80 @@ public class DecouverteDates {
 	}
 
 	public boolean estInferieurDateCourante(Date date) throws ParseException {
-		// TODO
-		throw new RuntimeException("à implémenter");
+		Date aujourdhui = new Date();
+		if (aujourdhui.before(date)) {
+			return false;
+		}
+		return true;
 	}
 
 	public Date construireDate(String chaineFournie) {
-		// TODO
-		throw new RuntimeException("à implémenter");
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = null;
+		try {
+			date = dateFormat.parse(chaineFournie);
+			System.out.println("Date parsée : " + date);
+
+		} catch (Exception e) {
+			System.err.println("Format de date invalide. Usage : dd/MM/YYYY");
+			System.err.println(e.getMessage());
+		}
+		return date;
+
+		// String date = "4";
+		// String month = "4";
+		// String year = "2017";
+		// String hourOfDay = "0";
+		// String minute ="0";
+		//
+		//
+		// Calendar cal = Calendar.getInstance();
+		// cal.clear();
+		//
+		// cal.set(year, month, date, hourOfDay, minute);
+
 	}
 
 	public Date augmenterDate(Date dateInitiale, int nombreJours,
 			int nombreHeures, int nombreMinutes) {
-		// TODO
-		throw new RuntimeException("à implémenter");
+
+		Calendar calendrier = Calendar.getInstance();
+		calendrier.setTime(dateInitiale);
+
+		calendrier.add(Calendar.DAY_OF_MONTH, nombreJours);
+		calendrier.add(Calendar.HOUR_OF_DAY, nombreHeures);
+		calendrier.add(Calendar.MINUTE, nombreMinutes);
+
+		Date datemodifie = calendrier.getTime();
+		return datemodifie;
+
 	}
 
 	public String formaterUneDate(Date date) {
-		// TODO
-		return "";
+	
+		SimpleDateFormat df = new SimpleDateFormat("dd_MM_yyyy");
+		String dateF = df.format(date.getTime());
+		return dateF;
 	}
 
 	public String formaterUneHeure(Date time) {
-		// TODO
-		return "";
+			
+		SimpleDateFormat hf = new SimpleDateFormat("HH");
+		SimpleDateFormat mf = new SimpleDateFormat("mm");
+		SimpleDateFormat sf = new SimpleDateFormat("ss");
+		String heureF = hf.format(time);
+		String minuteF = mf.format(time);
+		String secondeF =sf.format(time);
+		
+		return heureF + "h" +minuteF + "min" + secondeF + "sec";
 	}
-	
+
 	public String formaterDateEnFrancais(Date date) {
-		// TODO
-		return "";
+		
+		SimpleDateFormat dfr = new SimpleDateFormat("EEEE dd MMMM yyyy");
+		String dateFr = dfr.format(date);
+		return dateFr;
+		
 	}
 }
